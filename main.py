025ce581effe926a0413438697d584ef819fc68f
jaydenknibbs <129121370+jaydenknibbs@users.noreply.github.com>
2023-05-12 14:13:34 +1200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+# ask for an epik number
+get_number = int(input("chose a epik number ")) 
+
+#multiply it by 5
+times_five = get_number * 5
+
+#output the reesults
+print("{} times five is equal to {}.format(get_number, times_Five")
